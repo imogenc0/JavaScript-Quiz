d3dfd975cc58f90a1cb9c38a9f1cd6423c05d8aa
imogenc0 <imogencoughlin@gmail.com>
2023-11-07 22:09:38 +0000

    Adds event listener to clear starter code when the start button is clicked

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -11,9 +11,11 @@
 
 //Start
 var start = document.querySelector("#start");
+var wrapper = document.querySelector(".wrapper");
 
 
 // Question Variables Linked to HTML
+var questionsDiv = document.querySelector("#questions");
 var questionTitle = document.querySelector("#question-title");
 var answer1 = document.querySelector("#btn1");
 var answer2 = document.querySelector("btn2");
@@ -24,34 +26,26 @@ var answer5 = document.querySelector("#btn5");
 
 // object of questions
 var AllQuestions = {
-    Title: questions,
+    Title: questionsDiv,
     Options: allanswers
 }
 console.log(AllQuestions);
 
 // questions object
-var questions = {
+var questionsDiv = {
     question1: "What tag is used to link a JS file in HTML?",
     question2: "What can be stored in an array?",
-    question3 : "What must each object value have?",
-    question4 : "What would be the best way to store multiple related variables?"
+    question3: "What must each object value have?",
+    question4: "What would be the best way to store multiple related variables?"
 }
 console.log(questions)
 
-// 1 - What tag is used to link a JS file in HTML?
-// <body> / <meta> / <link> / <script> / <footer>
-// 2 - What can be stored in an array?
-// strings / numbers / boolean values / objects / all of the above
-// 3 - What must each object value have?
-// a key / a boolean statement / ; / matching values / a shelf
-// 4 - What would be the best way to store multiple related variables?
-// in an array / in an object / as a function / as a json / an eventlistener
 
 var allanswers = {
-    anstoq1 : ["<body>", "<meta>", "<link>", "<script>", "<footer>"],
-    anstoq2 : ["strings", "numbers", "boolean values", "objects", "all of the above"],
-    anstoq3 : ["a key", "a boolean statement", ";", "matching values", "a shelf"],
-    anstoq4 : ["in an array", "in an object", "as a function", "as a json", "as an Event.Listener"]
+    anstoq1: ["<body>", "<meta>", "<link>", "<script>", "<footer>"],
+    anstoq2: ["strings", "numbers", "boolean values", "objects", "all of the above"],
+    anstoq3: ["a key", "a boolean statement", ";", "matching values", "a shelf"],
+    anstoq4: ["in an array", "in an object", "as a function", "as a json", "as an Event.Listener"]
 }
 console.log(allanswers)
 console.log(allanswers.ans4)
@@ -67,6 +61,10 @@ console.log(correct)
 
 
 function startQuiz(event) {
+    console.log("Quiz Started");
+    wrapper.innerHTML = "";
+    showQuestion1();
+}
     // when button is clicked
     // hide start section
     // input new section and answers
@@ -74,13 +72,16 @@ function startQuiz(event) {
     // points++
     // else time - 5 seconds and display wrong
     // would it be worth only using if statement for if they answer wrong because then it would only take time off if answered wrong therefore no else would be required?
-}
+
 
 function showQuestion1(){
-    questionTitle.innerHTML = question1
-    question1.textcontent = AllQuestions.Title.question1
+    let questionTitle = questionsDiv.question1;
+    console.log(questionTitle);
+    questionTitle
+
 }
 
 start.addEventListener("click", startQuiz);
+
 
 
